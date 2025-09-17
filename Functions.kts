25f28d3c859ce80c301afa241aac8ae958111761
nickmugo <nicknjuru13@gmail.com>
2025-09-17 09:40:04 +0300

    pushing to github

--- Functions.kts
+++ Functions.kts
@@ -1,5 +1,52 @@
-fun main(args: Array<String>) {
-    println("Hello, world!")
+import java.util.*    // required import
+fun fishFood(day:String):String {
+    return when(day){
+        "Monday"-> "flakes"
+        "Tuesday"-> "pellets"
+        "Wednesday"->"redworms"
+        "Thursday"-> "granules"
+        "Friday"-> "mosquitoes"
+        "Saturday"-> "lettuce"
+        "Sunday"-> "plankton" 
+        else -> "nothing"            
+    }
 }
 
+fun main() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun swim(speed: String = "fast") {
+   println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" ->  true
+        else -> false
+    }
+}
+// Lambda function
+var dirtyLevel = 20
+val waterFilter = { dirty : Int -> dirty / 2}
+println(waterFilter(dirtyLevel))
+// passing a normal function as a parameter
+fun increaseDirty( start: Int ) = start + 1
+println(updateDirty(15, ::increaseDirty))
+swim()   // uses default speed
+swim("slow")   // positional argument
+swim(speed="turtle-like")   // named parameter
+val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+// val isUnit = println("This is an expression")
+// println(isUnit)
 // When a function does not explicitly have a return statement, the function returns Unit (formally kotlin.Unit)
+main()
--- Lists_arrays.kts
+++ Lists_arrays.kts
@@ -1,22 +1,54 @@
-fun main(){
+fun main() {
 
     val school = listOf("Elementary", "Middle", "High", "College")
     println(school)
+
     val myList = mutableListOf("tuna", "salmon", "shark")
     myList.remove("shark")
-    val school = arrayOf("shark", "salmon", "minnow")
-    println(java.util.Arrays.toString(school))
+
+    val fishArray = arrayOf("shark", "salmon", "minnow")
+    println(java.util.Arrays.toString(fishArray))
+
     val mix = arrayOf("fish", 2)
-    val numbers = intArrayOf(1,2,3)
-    val numbers = intArrayOf(1,2,3)
-    val numbers3 = intArrayOf(4,5,6)
-    val foo2 = numbers3 + numbers
+
+    val numbers1 = intArrayOf(1, 2, 3)
+    val numbers2 = intArrayOf(4, 5, 6)
+    val foo2 = numbers2 + numbers1
     println(foo2[5])
-    val numbers = intArrayOf(1, 2, 3)
+
     val oceans = listOf("Atlantic", "Pacific")
-    val oddList = listOf(numbers, oceans, "salmon")
+    val oddList = listOf(numbers1.toList(), oceans, "salmon")
     println(oddList)
-    val array = Array (5) { it * 2 }
+
+    val array = Array(5) { it * 2 }
     println(java.util.Arrays.toString(array))
+
+}
+
+fun filter() {
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    // eager, creates a new list
+    val eager = decorations.filter { it [0] == 'p' }
+    println("eager: $eager")
+       // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+        // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+    println("access: $it")
+    it
+    }
+    val result = lazyMap.toList()   // forces evaluation
+    println("result: $result")
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
 }
 main()
+filter()
